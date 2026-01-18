c9b0d572f327c05b991c5d12bb25e3967a1494cf
SK sanoyar ali <asksanoyar33@gmail.com>
2026-01-18 23:37:41 +0530

    added cors setup to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
 import messageRouter from './routes/message.route.js'
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  })
+)
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   console.log('root route')
   res.send('route working')
