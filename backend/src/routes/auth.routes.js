bcd381ea1a186587926d6f393ac38961521e308c
SK sanoyar ali <asksanoyar33@gmail.com>
2026-01-20 23:36:37 +0530

    fix:fix auth route names

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,8 +4,8 @@ const authRouter = express.Router()
 
 authRouter.post('/signup', signup)
 
-authRouter.post('/signup', login)
+authRouter.post('/login', login)
 
-authRouter.post('/signup', logout)
+authRouter.post('/logout', logout)
 
 export default authRouter
