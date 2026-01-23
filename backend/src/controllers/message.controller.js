86be4f316f0c1493213a5db2a96c4cae76ab4fe0
SK sanoyar ali <asksanoyar33@gmail.com>
2026-01-23 18:38:47 +0530

    added messages endpoints

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,125 @@
+import cloudinary from '../lib/cloudinary.js'
+import Message from '../models/message.model.js'
+import User from '../models/user.model.js'
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id
+
+    const contacts = await User.find({ _id: { $ne: loggedInUserId } }).select(
+      '-password'
+    )
+
+    return res.status(200).json(contacts)
+  } catch (error) {
+    console.log('Error in getAllContacts', error)
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server error',
+    })
+  }
+}
+
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id
+    const receiverId = req.params.id
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: receiverId },
+        { senderId: receiverId, receiverId: myId },
+      ],
+    })
+
+    return res.status(200).json(messages)
+  } catch (error) {
+    console.log('Error in getMessagesByUserId', error)
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server error',
+    })
+  }
+}
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, message } = req.body
+
+    const senderId = req.user._id
+    const { id: receiverId } = req.params
+    if (!text && !image) {
+      return res.status(400).json({
+        success: false,
+        message: 'text or image is required',
+      })
+    }
+
+    if (senderId.equals(receiverId)) {
+      return res.status(400).json({
+        success: false,
+        message: 'Cannot send messages to yourself',
+      })
+    }
+    const receiverExist = await User.exists({ _id: receiverId })
+    if (!receiverExist) {
+      return res.status(400).json({
+        success: false,
+        message: 'Receiver Not Found',
+      })
+    }
+
+    let imageUrl
+
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image)
+      imageUrl = uploadResponse.secure_url
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    })
+
+    res.status(201).json(newMessage)
+    // todo:send message in real time with socket.io
+  } catch (error) {
+    console.log('Error in Send message', error)
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server error',
+    })
+  }
+}
+
+export const getChatPartners = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id
+
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    })
+
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString === loggedInUserId
+            ? msg.receiverId.toString()
+            : msg.senderId.toString()
+        )
+      ),
+    ]
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select('-password')
+
+    res.status(200).json(chatPartners)
+  } catch (error) {
+    console.log('Error in Send message', error)
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server error',
+    })
+  }
+}
