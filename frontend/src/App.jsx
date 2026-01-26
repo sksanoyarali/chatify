72a1e973bbf97a3b6fc6479a199381ecb3e22099
SK sanoyar ali <asksanoyar33@gmail.com>
2026-01-26 19:44:42 +0530

    added signup ui and functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import ChatPage from './pages/ChatPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import { useAuthStore } from './store/useAuthStore'
+import PageLoader from './components/PageLoader'
+import { Toaster } from 'react-hot-toast'
 const App = () => {
-  return <div className=" text-5xl">App</div>
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  if (isCheckingAuth) return <PageLoader />
+  return (
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden ">
+      {/* DECORATORS - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500/20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500/20 blur-[100px]" />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <ChatPage /> : <Navigate to={'/login'} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={'/'} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={'/'} />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  )
 }
 
 export default App
